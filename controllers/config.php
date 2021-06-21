b0982d5498f6bdb0358f18e450881d055d518361
Verduzco Acevedo, F <f.verduzco.acevedo@accenture.com>
2021-06-20 21:33:29 -0500

    Heroku settings added

--- controllers/config.php
+++ controllers/config.php
@@ -1,13 +1,11 @@
 <?php
 session_start();
-// Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'pwainvoice';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-
-	exit('Fallo al conectar: ' . mysqli_connect_error());
-}
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.com"];
+$cleardb_username = $cleardb_url["bd80d75944e79f"];
+$cleardb_password = $cleardb_url["20fd46c8"];
+$cleardb_db = substr($cleardb_url["heroku_04e6c6923661b02"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
